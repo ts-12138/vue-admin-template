8f0724e02990245108f9bb2cd6e720ae6a87656d
TS <TS1951516375@qq.com>
2023-11-03 22:26:09 +0800

    feature:getInfo,登录界面

--- src/utils/request.js
+++ src/utils/request.js
@@ -14,12 +14,12 @@ const service = axios.create({
 service.interceptors.request.use(
   (config) => {
     // do something before request is sent
-
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers["X-Token"] = getToken();
+      console.log(getToken())
+      config.headers["authorization"] = getToken();
     }
     return config;
   },
@@ -43,10 +43,11 @@ service.interceptors.response.use(
    * You can also judge the status by HTTP Status Code
    */
   (response) => {
-    const res = response.data;
-    if (res.code && res.data != 200) {
+    const { code, data } = response.data;
+    console.log(data);
+    if (code && code != 200) {
       return Promise.reject(new Error(res.message || "Error"));
-    } else return res;
+    } else return data;
     // if the custom code is not 20000, it is judged as an error.
     // if ( res.code !== 20000) {
     //   Message({
